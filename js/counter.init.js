b9e6d53e84712b0ce30582853548cad70a96010a
zhanghuasheng <hebeblock@gmail.com>
2022-03-06 10:39:25 +0800

    Changes to be committed:

---
+++ js/counter.init.js
@@ -0,0 +1,36 @@
+var a = 0;
+$.ajax({url:"https://apis.hebe.cc/google/talkingdata",success:function(result){
+        console.log(JSON.parse(result).totaluser);
+        $("#dw").attr("data-count",JSON.parse(result).totaluser*3);
+        $("#usersnum").attr("data-count",JSON.parse(result).totaluser);
+
+        $(window).scroll(function() {
+            var oTop = $('#counter').offset().top - window.innerHeight;
+            if (a == 0 && $(window).scrollTop() > oTop) {
+                $('.counter-value').each(function() {
+                    var $this = $(this),
+                        countTo = $this.attr('data-count');
+                    $({
+                        countNum: $this.text()
+                    }).animate({
+                            countNum: countTo
+                        },
+
+                        {
+                            duration: 2000,
+                            easing: 'swing',
+                            step: function() {
+                                $this.text(Math.floor(this.countNum));
+                            },
+                            complete: function() {
+                                $this.text(this.countNum);
+                                //alert('finished');
+                            }
+
+                        });
+                });
+                a = 1;
+            }
+        });
+
+    }});
